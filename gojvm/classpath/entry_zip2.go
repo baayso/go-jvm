d006787f7a8117e3f975a15a0d116dc2b482b1be
baayso <baayso@outlook.com>
2018-09-02 11:00:18 +0800

    Update: entry_zip2.go

--- gojvm/classpath/entry_zip2.go
+++ gojvm/classpath/entry_zip2.go
@@ -12,6 +12,7 @@ type ZipEntry2 struct {
 
 func newZipEntry2(path string) *ZipEntry2 {
 	absPath, err := filepath.Abs(path)
+
 	if err != nil {
 		panic(err)
 	}
@@ -19,55 +20,67 @@ func newZipEntry2(path string) *ZipEntry2 {
 	return &ZipEntry2{absPath, nil}
 }
 
-func (self *ZipEntry2) readClass(className string) ([]byte, Entry, error) {
-	if self.zipRC == nil {
-		err := self.openJar()
+func (z *ZipEntry2) readClass(className string) ([]byte, Entry, error) {
+	if z.zipRC == nil {
+		err := z.openJar()
+
 		if err != nil {
 			return nil, nil, err
 		}
 	}
 
-	classFile := self.findClass(className)
+	classFile := z.findClass(className)
+
 	if classFile == nil {
 		return nil, nil, errors.New("class not found: " + className)
 	}
 
 	data, err := readClass(classFile)
-	return data, self, err
+
+	return data, z, err
 }
 
 // todo: close zip
-func (self *ZipEntry2) openJar() error {
-	r, err := zip.OpenReader(self.absPath)
+func (z *ZipEntry2) openJar() error {
+	r, err := zip.OpenReader(z.absPath)
+
 	if err == nil {
-		self.zipRC = r
+		z.zipRC = r
 	}
+
 	return err
 }
 
-func (self *ZipEntry2) findClass(className string) *zip.File {
-	for _, f := range self.zipRC.File {
+func (z *ZipEntry2) findClass(className string) *zip.File {
+	for _, f := range z.zipRC.File {
+
 		if f.Name == className {
 			return f
 		}
 	}
+
 	return nil
 }
 
 func readClass(classFile *zip.File) ([]byte, error) {
 	rc, err := classFile.Open()
+
 	if err != nil {
 		return nil, err
 	}
+
+	defer rc.Close()
+
 	// read class data
 	data, err := ioutil.ReadAll(rc)
-	rc.Close()
+
 	if err != nil {
 		return nil, err
 	}
+
 	return data, nil
 }
 
-func (self *ZipEntry2) String() string {
-	return self.absPath
+func (z *ZipEntry2) String() string {
+	return z.absPath
 }
